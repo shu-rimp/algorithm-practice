3109388c757d0576ac596b91121f55113b4f5d6c
Sooji Park <101962061+shurimpee@users.noreply.github.com>
2022-04-04 15:18:14 +0000

    leetcode two sum

---
+++ DataStructure/4.py
@@ -0,0 +1,13 @@
+#leetcode Two Sum
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:    # Don't edit
+        solution = []
+
+        for i in range(len(nums)) :
+            for j in range(i+1, len(nums)) :
+                if nums[i] + nums[j] == target :
+                    solution.append(i)
+                    solution.append(j)
+
+        return solution
--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 #main.py
 import os
-os.system('python3 DataStructure/3.py')
+os.system('python3 DataStructure/4.py')
