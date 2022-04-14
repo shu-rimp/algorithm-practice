6c83dab54a51781cf3be337e8879eb99e6064b11
shurimpee <soooojip@gmail.com>
2022-04-14 21:13:37 +0900

    Baekjoon17478 수정

--- Algorithm/src/implementation/Solution.java
+++ Algorithm/src/implementation/Solution.java
@@ -28,8 +28,9 @@ public class Solution {
 	
 	static void recursion(int n) throws Exception {
 		
+		bw.write(dash + QUOTES + "재귀함수가 뭔가요?" + QUOTES + LF);
+		
 		if(n == 0) {
-			bw.write(dash + QUOTES + "재귀함수가 뭔가요?" + QUOTES + LF);
 			bw.write(dash + QUOTES + "재귀함수는 자기 자신을 호출하는 함수라네" + QUOTES + LF);
 			bw.write(dash + "라고 답변하였지." + LF);
 			
@@ -38,7 +39,6 @@ public class Solution {
 			return;
 		} // if
 		
-		bw.write(dash + QUOTES + "재귀함수가 뭔가요?" + QUOTES + LF);
 		bw.write(dash + QUOTES + "잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어." + LF);
 		bw.write(dash + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지." + LF);
 		bw.write(dash + "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어." + QUOTES + LF);
@@ -47,6 +47,7 @@ public class Solution {
 		dash += "____";
 		
 		recursion(n-1);
+		
 	} // recursion
 	
 	static void recursion2(int n) throws Exception {
